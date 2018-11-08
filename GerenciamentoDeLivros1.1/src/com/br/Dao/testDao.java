01e6cd3e667d68e78a41a1899c308317a7b4b023
Reinaldo Ferreira Santos Junior <reinaldofsjunior@live.com>
2018-11-07 22:18:52 -0200

    Add files via upload
    Gerenciamento Livros 1.2 - Realizado testes de back-end Livro.
    Conexão de login funcionando.
    Falta(Back-End) Testar Dao Usuario, Emprestimo e Aluno.
    Falta(Front-End) Linkar as páginas e conectar com as Daos.

---
+++ GerenciamentoDeLivros1.1/src/com/br/Dao/testDao.java
@@ -0,0 +1,70 @@
+package com.br.Dao;
+
+import com.br.Models.Livro;
+
+public class testDao {
+	/*public static void main(String[] args) {
+		Livro l = new Livro();
+		
+		LivroDaoImpl d = null;
+		try {
+			d = new LivroDaoImpl();
+		} catch (GenericDaoException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		l.setId(2);
+		l.setNome("O ACHIMENTO");
+		l.setGenero("ficcao");
+		l.setAutor("Tara");
+		l.setEditora("Beby");
+		l.setSatus(true);
+		
+		try {
+			d.adiciona(l);
+			System.out.println("Funcionou");
+		} catch (GenericDaoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			d.remover(1);
+			System.out.println("Funcionou");
+		} catch (GenericDaoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			d.pesquisarPorAutor("Tara");
+			System.out.println("Funcionou");
+		} catch (GenericDaoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		try {
+			d.pesquisarPorId(2);
+			System.out.println("Funcionou");
+		} catch (GenericDaoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		try {
+			d.salvar(2, l);;
+			System.out.println("Funcionou");
+		} catch (GenericDaoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}*/
+}
